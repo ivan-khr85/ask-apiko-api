b20bd7d638b56d81e6645fefb1d79e80908ed24c
volodymyr <volodymyrtymets@gmail.com>
2018-05-15 16:47:36 +0300

    mome generation of db

--- on-start.js
+++ on-start.js
@@ -3,12 +3,10 @@ const {
 } = require('./src/scripts/onStart');
 
 
-
-
 const onAppStart = async () => {
   try {
     console.log('----> on start...');
-    await generateQuestions(1000);
+    // await generateQuestions(1000);
   } catch (error) {
     console.log('---> on start Error: ');
     console.log(error)
