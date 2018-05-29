28753e1d0b039fda2700f89013657ef5a0d96f8f
Ivan Seniv <seniv97@gmail.com>
2018-05-29 20:58:29 +0300

    show user questions in userinfo

--- src/controllers/users/my.js
+++ src/controllers/users/my.js
@@ -1,9 +1,13 @@
 const { sendOne } = require('../../middleware/index');
 
-const getMy = ({ User }) => async (req, res, next) => {
+const getMy = ({ User, Question }) => async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id, req.fields);
-    return sendOne(res, { user });
+    console.log(req.user.id)
+    const questions = await Question.find({ createdById: req.user.id });
+    console.log(questions)
+
+    return sendOne(res, { user, questions });
 
   } catch (error) {
     next(error);
