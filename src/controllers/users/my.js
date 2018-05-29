217384b529044622ca6c9ff337dc44449fb8ec49
Ivan Seniv <seniv97@gmail.com>
2018-05-29 20:59:14 +0300

    removed console logs

--- src/controllers/users/my.js
+++ src/controllers/users/my.js
@@ -3,9 +3,7 @@ const { sendOne } = require('../../middleware/index');
 const getMy = ({ User, Question }) => async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id, req.fields);
-    console.log(req.user.id)
     const questions = await Question.find({ createdById: req.user.id });
-    console.log(questions)
 
     return sendOne(res, { user, questions });
 
