d1ad6c6c806ec0ba8d0181de86a4618452d7a517
ivan-khr85 <ivan.khr85@gmail.com>
2018-05-30 13:15:00 +0300

    fix user questions

--- src/controllers/users/my.js
+++ src/controllers/users/my.js
@@ -2,8 +2,12 @@ const { sendOne } = require('../../middleware/index');
 
 const getMy = ({ User, Question }) => async (req, res, next) => {
   try {
+  	const sortQuery = { _id: -1 };
+
     const user = await User.findById(req.user.id, req.fields);
-    let questions = await Question.find({ createdById: req.user.id }).limit(10);
+    let questions = await Question.find({ createdById: req.user.id })
+    	.limit(10)
+    	.sort(sortQuery);
     return sendOne(res, { user, questions });
 
   } catch (error) {
